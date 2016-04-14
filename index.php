941dcff64a93370f255363d2630342538f0d3a4f
ameitor <blogdor.ammr@gmail.com>
2016-04-14 15:59:56 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="images/icon/favicon.ico" type="image/x-icon">
+	<!--Boostrap-->
+	<!--link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap-theme.min.css"-->
+	<!--link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap.min.css"-->
+	<title>Inicio - BLOGDOR</title>
+</head>
+
+
+<body>
+
+</body>
+</html>
